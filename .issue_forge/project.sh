6434f4c5f97db615f1267ba866b9369d08700422
ozw4 <zerowarurei@gmail.com>
2026-04-22 13:42:31 +0000

    chore: update project.sh with flow base branch and ref

--- .issue_forge/project.sh
+++ .issue_forge/project.sh
@@ -1,2 +1,4 @@
 # seisviewer issue_forge consumer config.
 # Defaults are supplied by vendor/issue_forge.
+CODEX_FLOW_BASE_BRANCH='feature/time_measurements'
+CODEX_FLOW_BASE_REF='origin/feature/time_measurements'
