368d56364f03377fbde51da01c0f39b187d3a9f4
ozw4 <zerowarurei@gmail.com>
2026-04-27 04:42:17 +0000

    update:project.sh

--- .issue_forge/project.sh
+++ .issue_forge/project.sh
@@ -1,4 +1,4 @@
 # seisviewer issue_forge consumer config.
 # Defaults are supplied by vendor/issue_forge.
-CODEX_FLOW_BASE_BRANCH='main'
-CODEX_FLOW_BASE_REF='origin/main'
+CODEX_FLOW_BASE_BRANCH='feature/lmo_view'
+CODEX_FLOW_BASE_REF='origin/feature/lmo_view'
