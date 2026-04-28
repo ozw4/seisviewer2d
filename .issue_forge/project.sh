51f4ebaf03d306af6abd92a69622c72a3b8d3213
ozw4 <zerowarurei@gmail.com>
2026-04-28 13:48:42 +0000

    update: project.sh

--- .issue_forge/project.sh
+++ .issue_forge/project.sh
@@ -1,4 +1,4 @@
 # seisviewer issue_forge consumer config.
 # Defaults are supplied by vendor/issue_forge.
-CODEX_FLOW_BASE_BRANCH='main'
-CODEX_FLOW_BASE_REF='origin/main'
+CODEX_FLOW_BASE_BRANCH='feature/static_correction'
+CODEX_FLOW_BASE_REF='origin/feature/static_correction'
